ced48e511d9eceaccd212f7d336e67d9a5d444fd
Deja Jackson <bananafudgkins@gmail.com>
2024-03-07 09:12:41 -0800

    Initial Commit

---
+++ FinDash/FinDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinDashApp.swift
+//  FinDash
+//
+//  Created by Deja Jackson on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FinDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
